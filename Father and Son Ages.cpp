60fc5a76ec085862f3c4ac6d102e1b3ff7e88a6e
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-06-26 10:38:06 +0600

    Create Father and Son Ages.cpp

---
+++ Father and Son Ages.cpp
@@ -0,0 +1,11 @@
+// Problem Statement
+// Write a program to create a function that takes two arguments: a father's current age fAge and his son's current age sAge. Сalculate how many years ago the father was twice as old as his son, or in how many years he will be twice as old.
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    int fAge, sAge;
+    cin>> fAge >> sAge;
+    cout<< abs(fAge-(sAge*2));
+    return 0;
+}
