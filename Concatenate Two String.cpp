3442035f2c3cc495a38f4a17e93fdf0be81054e1
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-07-04 13:47:22 +0600

    Create Concatenate Two String.cpp

---
+++ Concatenate Two String.cpp
@@ -0,0 +1,11 @@
+// Problem Statement
+// Write a program where two strings will be given, firstName and lastName, return a single string in the format "lastName, firstName".
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    string firstName, lastName;
+    cin>> firstName >> lastName;
+    cout<<lastName<<", "<<firstName<<endl;
+    return 0;
+}
