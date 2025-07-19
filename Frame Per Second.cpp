6fe4571febb7721222902aed46123e1cdf40de7a
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-07-19 23:39:41 +0600

    Create Frame Per Second.cpp

---
+++ Frame Per Second.cpp
@@ -0,0 +1,16 @@
+// Problem Statement
+// Write a program that returns the number of frames shown in a given number
+// of minutes for a certain FPS. FPS stands for "frames per second" and it's the number of frames a computer screen shows every second.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int minutes, fps;
+    cin >> minutes >> fps;
+
+    int totalFrames = minutes * 60 * fps;
+    cout << totalFrames << endl;
+
+    return 0;
+}
