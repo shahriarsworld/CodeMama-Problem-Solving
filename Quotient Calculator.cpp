3c66e7ff41261d448d7571777d306e058cfa30a7
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-08-01 23:28:51 +0600

    Create Quotient Calculator.cpp

---
+++ Quotient Calculator.cpp
@@ -0,0 +1,13 @@
+// Problem Statement
+// Write a program to find the quotient of two numbers.
+
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    int x,y;
+    cin>> x >> y;
+    cout<< x/y <<endl;
+
+    return 0;
+}
