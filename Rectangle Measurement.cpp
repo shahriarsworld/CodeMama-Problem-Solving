0565718bd0c709758e146e7b594557f8b8b58339
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-08-06 22:57:56 +0600

    Create Rectangle Measurement.cpp

---
+++ Rectangle Measurement.cpp
@@ -0,0 +1,13 @@
+// Problem Statement
+// Write a program to calculate the area of a rectangle.
+
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    int l, w;
+    cin>>l >> w;
+    cout<<l*w<<endl;
+
+    return 0;
+}
