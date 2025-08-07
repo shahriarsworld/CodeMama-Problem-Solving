9322a6b8c27d9d433c831b1318fc95b5bce83566
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-08-07 23:39:29 +0600

    Update Frame Per Second.cpp

--- Frame Per Second.cpp
+++ Frame Per Second.cpp
@@ -1,7 +1,6 @@
 // Problem Statement
 // Write a program that returns the number of frames shown in a given number
 // of minutes for a certain FPS. FPS stands for "frames per second" and it's the number of frames a computer screen shows every second.
-
 #include <iostream>
 using namespace std;
 
