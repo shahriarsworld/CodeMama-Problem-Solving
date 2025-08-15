6c34b6727d557b6245bd8244ea143e690bb35433
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-08-15 23:50:00 +0600

    Create Triangle Measurement.cpp

---
+++ Triangle Measurement.cpp
@@ -0,0 +1,13 @@
+// Problem Statement
+// Write a program to calculate the area of a Triangle.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int base, height;
+    cin >> base >> height;
+    int area = (base * height) / 2;
+    cout << area << endl;
+    return 0;
+}
